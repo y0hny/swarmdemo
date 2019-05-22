fe7c1efbaca7e37f8d1398d0855b3a3178ead4cc
root <root@azurevm.tt4rmxjedyxuhf2egcu2rlj22d.jx.internal.cloudapp.net>
2019-05-22 15:29:54 +0000

    Source changed

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ redis = Redis(host='redis', port=6379)
 def hello():
     count = redis.incr('hits')
     hostname = socket.gethostname()
-    return 'Hello World! I have been seen {} times.\n'.format(count) + "Running on host" + hostname
+    return 'NEW Version Hello World! I have been seen {} times.\n'.format(count) + "Running on host" + hostname
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
